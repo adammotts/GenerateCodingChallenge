f2168cbf5ad550a6230876070edb72a2e0582e8f
Adam Ma <adam.zym@gmail.com>
2023-12-22 10:43:03 -0500

    Tests

--- tests.py
+++ tests.py
@@ -30,17 +30,25 @@ def tests():
     assert parse_barcode(r"#1%^!2#") == r"12"
     
     
-    # Reverse and Encrypt Symbol Placement Does Not Matter (Only Order Of Operations)
+    # Reverse and Encrypt Symbol Placement Does Not Matter (Order Of Operations)
     assert parse_barcode(r"#12#3^4#") == r"2134"
     assert parse_barcode(r"#12#3%4#") == r"2434"
     assert parse_barcode(r"#12#3^^%%4#") == r"4834"
     
-    # Repeat Symbol Placement Does Matter
+    # Repeat Symbol Placement Does Matter (Order Of Operations)
     assert parse_barcode(r"#12#!34#") == r"121234"
     assert parse_barcode(r"#12#3!4#") == r"123124"
     assert parse_barcode(r"#12#34!#") == r"123412"
     
-    # Data Blocks Are Permanently Mutated For Future Usage (Basically Just Order Of Operations)
+    # Data Blocks Are Permanently Mutated For Future Usage (Order Of Operations)
     assert parse_barcode(r"#12#^34!#") == r"213421"
     assert parse_barcode(r"#12#%34!#") == r"243424"
     assert parse_barcode(r"#12#^3!4%5!#") == r"423214542"
+    
+    # Empty Barcodes Return Nothing
+    assert parse_barcode(r"") == r""
+    
+    # Barcodes With Nothing Return Nothing
+    assert parse_barcode(r"#") == r""
+    assert parse_barcode(r"##") == r""
+    assert parse_barcode(r"###") == r""
